e20cbe68250da01562f64a0d9f9424e391aebf1e
Lily <lolpo12345@outlook.com>
2022-03-25 19:20:31 +0300

    Create base style for navbar

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,11 @@ import "../../Assets/Stylesheets/Navbar.css";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>Navbar</h1>
+      <h1 className="navbar-logo">Jobify</h1>
+      <h2>
+        <em>Welcome!</em>
+      </h2>
+      <h1 className="navbar-settings">Settings</h1>
     </nav>
   );
 };
