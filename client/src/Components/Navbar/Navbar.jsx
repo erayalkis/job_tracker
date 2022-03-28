8bc76ad341e84b19492a5317e940ea01254dbdc0
Lily <lolpo12345@outlook.com>
2022-03-29 02:51:15 +0300

    Remove unnecessary element

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -4,10 +4,6 @@ const Navbar = ({ editing, setIsEditing }) => {
   return (
     <nav className="navbar">
       <h1 className="navbar-logo">Jobify</h1>
-
-      <h1 className="navbar-settings">
-        {editing ? "Leave Edit Mode" : "Edit Jobs"}
-      </h1>
     </nav>
   );
 };
